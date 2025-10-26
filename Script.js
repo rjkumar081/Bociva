609ed26e523181500845f4932037cd931a7b7797
rjkumar081 <97723829+rjkumar081@users.noreply.github.com>
2025-10-26 13:57:23 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,5 +2,5 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navUl = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', () => {
-    navUl.classList.toggle('show');
+  navUl.classList.toggle('show');
 });
