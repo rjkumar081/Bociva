ec6f4f0c3fe1cd3ffce1e769ca8b90050a4c1b8d
rjkumar081 <97723829+rjkumar081@users.noreply.github.com>
2025-10-27 16:01:19 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navUl = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-  navUl.classList.toggle('show');
-});
