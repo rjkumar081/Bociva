638e914dd9aa67411c0c5448aedf2d22ddaf92d8
rjkumar081 <97723829+rjkumar081@users.noreply.github.com>
2025-10-27 16:16:40 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ⚠️ IMPORTANT: base must match your repo name exactly (case-sensitive)
-export default defineConfig({
-  plugins: [react()],
-  base: '/MeraPopi-Digital-Seva-Center/',
-})
